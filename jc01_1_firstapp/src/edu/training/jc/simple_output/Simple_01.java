542a680b59a5e2e364bde9596622bf6f26a5108d
avdeeva9292 <avdeeva9292@gmail.com>
2024-10-03 00:20:48 +0300

    Add files via upload

---
+++ jc01_1_firstapp/src/edu/training/jc/simple_output/Simple_01.java
@@ -0,0 +1,10 @@
+package edu.training.jc.simple_output;
+
+public class Simple_01 {
+
+	public static void main(String[] args) {
+		System.out.println("A\nB\nC\nD\nE");
+
+	}
+
+}
---
+++ jc01_1_firstapp/src/edu/training/jc/simple_output/StairsOutput.java
@@ -0,0 +1,18 @@
+package edu.training.jc.simple_output;
+
+public class StairsOutput {
+
+	public static void main(String[] args) {
+		System.out.println("\"To wisely live your life, you don't need to know much");
+		System.out.println(" ");
+		System.out.println("\tJust remember two main rules for the beginning:");
+		System.out.println(" ");
+		System.out.println("\t\tYou better starve, than eat whatever");
+		System.out.println(" ");
+		System.out.println("\tAnd better be alone, than with whoever.\"");
+		System.out.println(" ");
+		System.out.println("Omar Khayyam, Rubaiyat");
+
+	}
+
+}
